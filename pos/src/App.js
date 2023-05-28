eb75d2bfe5eda049bfa9569594531868e46718b8
seiyoon <sera4084@naver.com>
2023-05-28 22:55:03 +0900

    초기화 후 리액트 다시 세팅

--- pos/src/App.js
+++ pos/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
+import { First } from "./pages/First";
+import { Home } from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<First />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ pos/src/pages/First.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const First = () => {
+  return (
+    <StFirst>
+      <StFirstContainer></StFirstContainer>
+    </StFirst>
+  );
+};
+
+const StFirst = styled.div``;
+const StFirstContainer = styled.div``;

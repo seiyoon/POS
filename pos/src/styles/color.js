bdb5fbede2eed391a1eb06f43e4eb817eb1dff7c
seiyoon <sera4084@naver.com>
2023-05-29 00:05:14 +0900

    폰트 추가 & 색 선정

---
+++ pos/src/styles/color.js
@@ -0,0 +1,10 @@
+export const COLOR = {
+  MAIN_WHITE: "#ffffff",
+  MAIN_BLACK: "#121212",
+  MAIN: "#EB5844",
+  MAIN_HOVER: "#EC7B6B",
+  BRIGHT_GRAY: "#EEEEEE",
+  LIGHT_GRAY: "#DEDEDE",
+  MEDIUM_GRAY: "#9D9D9D",
+  DARK_GRAY: "#535353",
+};

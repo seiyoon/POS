7b940165628501011d9d7bc73cc131573db945ee
seiyoon <sera4084@naver.com>
2023-05-29 11:35:13 +0900

    feat: 버튼 클릭시 선택메뉴 보임 & 삭제가능

--- pos/src/pages/Open.js
+++ pos/src/pages/Open.js
@@ -4,61 +4,111 @@ import { COLOR } from "../styles/color";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/pos_Logo.svg";
+import { ReactComponent as Trash } from "../assets/trash.svg";
 
 function Open() {
   const [category, setCategory] = useState("음료");
+  const [selectedMenus, setSelectedMenus] = useState([]);
+
   const categories = ["음료", "아이스크림", "디저트", "기타"];
 
   const ClickCategory = (c) => {
     setCategory((prevCategory) => (prevCategory === c ? "" : c));
-    window.scrollTo(0, 0);
+  };
+  const handleMenuClick = (menu) => {
+    const existingMenu = selectedMenus.find(
+      (selectedMenu) => selectedMenu.id === menu.id
+    );
+
+    if (existingMenu) {
+      const updatedMenus = selectedMenus.map((selectedMenu) => {
+        if (selectedMenu.id === menu.id) {
+          return {
+            ...selectedMenu,
+            quantity: selectedMenu.quantity + 1,
+            totalPrice: selectedMenu.totalPrice + menu.price,
+          };
+        }
+        return selectedMenu;
+      });
+
+      setSelectedMenus(updatedMenus);
+    } else {
+      setSelectedMenus((prevSelectedMenus) => [
+        ...prevSelectedMenus,
+        { ...menu, quantity: 1, totalPrice: menu.price },
+      ]);
+    }
+  };
+
+  const handleMenuDelete = (menu) => {
+    setSelectedMenus((prevSelectedMenus) => {
+      const updatedMenus = prevSelectedMenus
+        .map((selectedMenu) => {
+          if (selectedMenu.id === menu.id) {
+            if (selectedMenu.quantity > 1) {
+              return {
+                ...selectedMenu,
+                quantity: selectedMenu.quantity - 1,
+                totalPrice: selectedMenu.totalPrice - menu.price,
+              };
+            }
+            // 수량이 1일 경우, 해당 메뉴를 제거
+            return null;
+          }
+          return selectedMenu;
+        })
+        .filter(Boolean); // null인 항목 제거
+
+      return updatedMenus;
+    });
   };
 
   const Beveragemenu = [
-    { name: "아메리카노", price: "3,000₩" },
-    { name: "카페라떼", price: "3,500₩" },
-    { name: "카라멜라떼", price: "4,500₩" },
-    { name: "바닐라라떼", price: "4,500₩" },
-    { name: "라임에이드", price: "6,000₩" },
-    { name: "자몽에이드", price: "6,000₩" },
-    { name: "레몬에이드", price: "6,000₩" },
-    { name: "청포도에이드", price: "6,000₩" },
-    { name: "초코라떼", price: "5,000₩" },
-    { name: "녹차라떼", price: "5,500₩" },
-    { name: "생딸기라떼", price: "5,500₩" },
+    { id: 1, name: "아메리카노", price: 3000 },
+    { id: 2, name: "카페라떼", price: 3500 },
+    { id: 3, name: "카라멜라떼", price: 4500 },
+    { id: 4, name: "바닐라라떼", price: 4500 },
+    { id: 5, name: "라임에이드", price: 6000 },
+    { id: 6, name: "자몽에이드", price: 6000 },
+    { id: 7, name: "레몬에이드", price: 6000 },
+    { id: 8, name: "청포도에이드", price: 6000 },
+    { id: 9, name: "초코라떼", price: 5000 },
+    { id: 10, name: "녹차라떼", price: 5500 },
+    { id: 11, name: "생딸기라떼", price: 5500 },
   ];
   const Icecreammenu = [
-    { name: "초코", price: "3,500₩" },
-    { name: "딸기", price: "3,500₩" },
-    { name: "녹차", price: "3,500₩" },
-    { name: "바닐라", price: "3,500₩" },
-    { name: "레몬", price: "3,500₩" },
-    { name: "블루베리", price: "3,500₩" },
-    { name: "체리", price: "3,500₩" },
-    { name: "자몽", price: "3,500₩" },
-    { name: "초코 젤라또", price: "4,500₩" },
-    { name: "딸기 젤라또", price: "4,500₩" },
-    { name: "녹차 젤라또", price: "4,500₩" },
-    { name: "쌀 젤라또", price: "4,500₩" },
-    { name: "자몽 젤라또", price: "4,500₩" },
-    { name: "레몬 젤라또", price: "4,500₩" },
-    { name: "블루베리 젤라또", price: "4,500₩" },
-    { name: "피스타치오 젤라또", price: "4,500₩" },
-    { name: "바닐라 젤라또", price: "4,500₩" },
-    { name: "라임 젤라또", price: "4,500₩" },
+    { id: 14, name: "초코", price: 3500 },
+    { id: 15, name: "딸기", price: 3500 },
+    { id: 16, name: "녹차", price: 3500 },
+    { id: 17, name: "바닐라", price: 3500 },
+    { id: 18, name: "레몬", price: 3500 },
+    { id: 19, name: "블루베리", price: 3500 },
+    { id: 20, name: "체리", price: 3500 },
+    { id: 21, name: "자몽", price: 3500 },
+    { id: 22, name: "초코 젤라또", price: 4500 },
+    { id: 23, name: "딸기 젤라또", price: 4500 },
+    { id: 24, name: "녹차 젤라또", price: 4500 },
+    { id: 25, name: "쌀 젤라또", price: 4500 },
+    { id: 26, name: "자몽 젤라또", price: 4500 },
+    { id: 27, name: "레몬 젤라또", price: 4500 },
+    { id: 28, name: "블루베리 젤라또", price: 4500 },
+    { id: 29, name: "피스타치오 젤라또", price: 4500 },
+    { id: 30, name: "바닐라 젤라또", price: 4500 },
+    { id: 31, name: "라임 젤라또", price: 4500 },
   ];
-  const Breadmenu = [
-    { name: "마들렌", price: "3,500₩" },
-    { name: "초코 마들렌", price: "3,800₩" },
-    { name: "녹차 마들렌", price: "3,800₩" },
-    { name: "휘낭시에", price: "3,500₩" },
-    { name: "조각케이크", price: "6,800₩" },
-    { name: "홀케이크", price: "42,000₩" },
+  const Dessertmenu = [
+    { id: 32, name: "마들렌", price: 3500 },
+    { id: 33, name: "초코 마들렌", price: 3800 },
+    { id: 34, name: "녹차 마들렌", price: 3800 },
+    { id: 35, name: "휘낭시에", price: 3500 },
+    { id: 36, name: "조각케이크", price: 6800 },
+    { id: 37, name: "홀케이크", price: 42000 },
   ];
   const Etcmenu = [
-    { name: "생수", price: "1,500₩" },
-    { name: "숫자초", price: "800₩" },
-    { name: "캐릭터초", price: "2,000₩" },
+    { id: 38, name: "생수", price: 1500 },
+    { id: 39, name: "숫자초", price: 800 },
+    { id: 40, name: "캐릭터초", price: 2000 },
   ];
 
   return (
@@ -69,7 +119,21 @@ function Open() {
         </Link>
       </StOpenHeader>
       <StOpenContainer>
-        <LeftOpenContainer></LeftOpenContainer>
+        <LeftOpenContainer>
+          <MenuList>
+            {selectedMenus.map((menu) => (
+              <SelectedMenu key={menu.name}>
+                <div className="name">{menu.name}</div>
+                <div className="price">{menu.totalPrice}원</div>
+                <div className="quantity">{menu.quantity}</div>
+                <Trash
+                  className="trash"
+                  onClick={() => handleMenuDelete(menu)}
+                />
+              </SelectedMenu>
+            ))}
+          </MenuList>
+        </LeftOpenContainer>
         <RightOpenContainer>
           <OpenCategory>
             {categories.map((c) => (
@@ -86,9 +150,12 @@ function Open() {
             <Menu>
               <MenuGrid>
                 {Beveragemenu.map((item) => (
-                  <MenuButton key={item.name}>
+                  <MenuButton
+                    key={item.id}
+                    onClick={() => handleMenuClick(item)}
+                  >
                     <MenuName>{item.name}</MenuName>
-                    <MenuPrice>{item.price}</MenuPrice>
+                    <MenuPrice>{item.price}원</MenuPrice>
                   </MenuButton>
                 ))}
               </MenuGrid>
@@ -98,9 +165,12 @@ function Open() {
             <Menu>
               <MenuGrid>
                 {Icecreammenu.map((item) => (
-                  <MenuButton key={item.name}>
+                  <MenuButton
+                    key={item.id}
+                    onClick={() => handleMenuClick(item)}
+                  >
                     <MenuName>{item.name}</MenuName>
-                    <MenuPrice>{item.price}</MenuPrice>
+                    <MenuPrice>{item.price}원</MenuPrice>
                   </MenuButton>
                 ))}
               </MenuGrid>
@@ -109,10 +179,13 @@ function Open() {
           {category === "디저트" && (
             <Menu>
               <MenuGrid>
-                {Breadmenu.map((item) => (
-                  <MenuButton key={item.name}>
+                {Dessertmenu.map((item) => (
+                  <MenuButton
+                    key={item.id}
+                    onClick={() => handleMenuClick(item)}
+                  >
                     <MenuName>{item.name}</MenuName>
-                    <MenuPrice>{item.price}</MenuPrice>
+                    <MenuPrice>{item.price}원</MenuPrice>
                   </MenuButton>
                 ))}
               </MenuGrid>
@@ -122,9 +195,12 @@ function Open() {
             <Menu>
               <MenuGrid>
                 {Etcmenu.map((item) => (
-                  <MenuButton key={item.name}>
+                  <MenuButton
+                    key={item.id}
+                    onClick={() => handleMenuClick(item)}
+                  >
                     <MenuName>{item.name}</MenuName>
-                    <MenuPrice>{item.price}</MenuPrice>
+                    <MenuPrice>{item.price}원</MenuPrice>
                   </MenuButton>
                 ))}
               </MenuGrid>
@@ -156,19 +232,64 @@ const StOpenHeader = styled.div`
 `;
 const StOpenContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
   height: 100%;
 `;
 const LeftOpenContainer = styled.div`
   float: left;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
   width: 40%;
-  height: 100%;
-  background: black;
+`;
+const MenuList = styled.div`
+  overflow: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 600px;
+`;
+const SelectedMenu = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  align-items: center;
+  width: 80%;
+  padding: 20px 30px;
+  border-bottom: 1px solid ${COLOR.LIGHT_GRAY};
+
+  &:first-child {
+    border-top: none;
+  }
+  &:last-child {
+    border-bottom: none;
+  }
+
+  .name {
+    font-family: "SUIT Variable";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 19px;
+    letter-spacing: 0.5px;
+  }
+  .quantity {
+    font-family: "SUIT Variable";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 19px;
+    letter-spacing: 0.5px;
+    color: ${COLOR.MAIN};
+  }
+  .price {
+    font-family: "SUIT Variable";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 17px;
+    letter-spacing: 0.5px;
+  }
+  .trash {
+    width: 22px;
+    height: 22px;
+  }
 `;
 
 const RightOpenContainer = styled.div`
